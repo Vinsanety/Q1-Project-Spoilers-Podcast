ac5d5be865d85a72e46126027dc4aa1a7757affb
vince vahala <vincevahala@gmail.com>
2016-06-22 16:41:02 -0600

    Wednesday project day, pre-standdown overal commit. Made changes to the ajax call for appending index with most recent soundcloud track. Need to find way to access iframe embeded urls to write function that pulls most recent track. Fixed the jQuery on pulling in most recent title below the track on index. Other notes and changes too

--- js/main.js
+++ js/main.js
@@ -1,23 +1,26 @@
-var div = document.getElementById('trackInfo');
-
+//This gets the JSON from soundcloud and accesses the first track of the playlist "Website" and takes that title and appends the DOM with the title
 $.getJSON("https://api.soundcloud.com/playlists/235506624?client_id=f4f2237e0ee1500764af3532c6bc5e13",function(data){
-  div.innerHTML = 'Our Latest Spoiled Film: ' + data.tracks[0].title
+  $('#trackInfo').html('Our Latest Spoiled Film: ' + data.tracks[0].title)
   console.log("Mydatais:",data);
   // data.preventDefault();
 })
 
 
 
+//below will add the most recent tracks.. thinking of adding a function where the url is that will always push the most recent to site:
+$('#iframe').attr('src', 'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/266641313&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true')
 
 
-//Taken from the SoundCloud Documentation website:
-// initialize({
+
+
+
+// from soundcloud site:
+// SC.initialize({
 //   client_id: 'f4f2237e0ee1500764af3532c6bc5e13'
 // });
-// var url = 'https://api.soundcloud.com/playlists/235506624?client_id=f4f2237e0ee1500764af3532c6bc5e13';
 //
-// oEmbed(url, {
-//   auto_play: true }).then(function(oEmbed) {
+// var track_url = 'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/266641313&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true';
+// SC.oEmbed(track_url, { auto_play: false }).then(function(oEmbed) {
 //   div.innerHTML = oEmbed
 //   console.log('oEmbed response: ', oEmbed);
 // });
