c98f371d9c911729673469a40c76a95b0703e89d
vince vahala <vincevahala@gmail.com>
2016-06-24 12:00:23 -0600

    Got the soundcloud api call to extract title of our tracks and inside that call, called the omdb  api and matched them to extract and pull in the Movie poster to the DOM. Friday project week Pre-lunch work

--- js/tracks.js
+++ js/tracks.js
@@ -6,15 +6,26 @@ $.getJSON('https://api.soundcloud.com/playlists/235506624?client_id=f4f2237e0ee1
   $(data).each(function(index) {
     if(index === total-1){
       var lastTrack = data[index];
-      var titleSplice = lastTrack.title.substring(0,5);
-      console.log(titleSplice);
-        // if (titleSplice === ) {
-        //
-        // }
-
-
       console.log(lastTrack);
-      $('#ratingsOne').html(lastTrack.title)
+      var title = lastTrack.title;
+      console.log(title);
+      var titleSplit = title.split(' -');
+      console.log(titleSplit);
+      console.log(titleSplit[0]);
+      // if (titleSplit[0] === ) {
+      //
+      // }
+
+      //adds the title of most recent track to the DOM
+      $('#ratingsOne').html(titleSplit[0])
+
+      // Accessing OMDB for movie posters (using batman to start with):
+      $.getJSON('http://www.omdbapi.com/?s='+titleSplit[0], function (movie) {
+        console.log('http://www.omdbapi.com/?s=' + titleSplit[0]);
+        $('#posterOne').attr('src',movie.Search[0].Poster);
+      })
+
+
       console.log(lastTrack.id);
     }else if (index === total-2) {
       var lastTrack = data[index]
@@ -29,12 +40,6 @@ $.getJSON('https://api.soundcloud.com/playlists/235506624?client_id=f4f2237e0ee1
   $('#iframe').attr('src','https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/235506624&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true')
 })
 
-//Accessing OMDB for movie posters (using batman to start with):
-$.getJSON('http://www.omdbapi.com/?s=batman', function (movie) {
-  $('#posterOne').attr('src', movie.Search[0].Poster)
-  // console.log(movie.Search[0].Poster);
-  // console.log(movie);
-})
 
 //Nmuta suggestion:
 // var sorted = tracks.sort(function(a,b){ return a.id+b.id });
