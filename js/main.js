c5101d81fef3057999867b3dd7d95a3938888f55
vince vahala <vincevahala@gmail.com>
2016-06-26 17:31:13 -0600

    completely done, formatted, and styled! All jQuery works great. Next step is to take it live via firebase

--- js/main.js
+++ js/main.js
@@ -6,17 +6,21 @@ $.getJSON("https://api.soundcloud.com/playlists/235506624?client_id=f4f2237e0ee1
   $(data).each(function(index) {
     if (index === (total-1)){
       var lastTrack = data[index]
-      // console.log(lastTrack.title);
-
+      var title = lastTrack.title;
+      var titleSplit = title.split(' -');
       //this part pushes the title of the latest track to the DOM
       $('#trackInfo').html('Our Latest Spoiled Film: ' + lastTrack.title)
       //this part pushes the embedded url to the iframe div / DOM
       $('#iframe').attr('src', 'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/'+lastTrack.id+ '&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true')
+
+      //adding the poster, rating, plot under the latest episode
+      $.getJSON('http://www.omdbapi.com/?t='+titleSplit[0], function (movie) {
+        $('#poster').attr('src',movie.Poster);
+        $('#rating').html('IMDB Rating: '+movie.imdbRating);
+        $('#plot').html('Plot: '+movie.Plot);
+      })
     }
   })
   // console.log("Mydatais:",data)
   // data.preventDefault();
 })
-
-
- //This is the Lobster ep: 'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/265584726&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true'
